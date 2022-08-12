003525194dcd93a30fd4716dbaa07ccb5ebefaf0
Najma Abdi <anajmabdi@gmail.com>
2022-08-12 12:56:31 -0700

    switching from radio to select box because code got too complicated

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 //Will reveal object as soon as the browser loads
 
-window.onload = function() {
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
-    const question1 = document.getElementById("question1").value;
-    const question2 = document.getElementById("question2").value;
-    const question3 = document.getElementById("question3").value;
-    const question4 = document.getElementById("question4").value;
-    const question5 = document.getElementById("question5").value;
-  }
+function letsTest(event) {
+  event.preventDefault();
+  const question1 = document.getElementById("question1").value;
+  const question2 = document.getElementById("question2").value;
+  const question3 = document.getElementById("question3").value;
+  const question4 = document.getElementById("question4").value;
+  const question5 = document.getElementById("question5").value;
+  const option = document.getElementById("input[name = 'option']:checked").value;
+  console.log(checking);
 }
